05df6a940dbfd556412cd3667bc61fa32b8951e7
Jacob Fugal <jacob@instructure.com>
2012-03-07 18:20:47 -0700

    rm existing MRA before re-cloning in hudson
    Change-Id: Ia94c6d6bfc6f4a267902101d26a9a2ffb74a9186
    Reviewed-on: https://gerrit.instructure.com/9296
    Reviewed-by: Jacob Fugal <jacob@instructure.com>
    Tested-by: Jacob Fugal <jacob@instructure.com>

--- spec_canvas/hudson_setup.sh
+++ spec_canvas/hudson_setup.sh
@@ -5,5 +5,9 @@ set -o errexit
 # force correct plugin name
 mv vendor/plugins/canvalytics vendor/plugins/analytics
 
+set +e
+rm -rf "vendor/plugins/multiple_root_accounts"
+set -e
+
 # checkout MRA plugin since we depend on it
 git clone "ssh://hudson@gerrit.instructure.com:29418/multiple_root_accounts.git" "vendor/plugins/multiple_root_accounts"
