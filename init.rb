58c293804a323c0aeb568070af607ffccebb9625
Brian Whitmer <brian@instructure.com>
2012-06-05 20:56:21 -0600

    make canvalytics work in dev env w/o cache_classes
    test plan:
    - load a canvas page in the dev environment
    - confirm that all the data returns, even on reload
    
    - repeat in a production environemnt
    
    Change-Id: I7ec81d9b1dddf862925efc7138485848b2ca13a6
    Reviewed-on: https://gerrit.instructure.com/11343
    Reviewed-by: Jacob Fugal <jacob@instructure.com>
    Tested-by: Jenkins <jenkins@instructure.com>

--- init.rb
+++ init.rb
@@ -1,3 +1,5 @@
+Canvas.reloadable_plugin(File.dirname(__FILE__))
+
 controller_path = File.dirname(__FILE__)+'/app/controllers'
 ActiveSupport::Dependencies.autoload_paths.delete controller_path
 ActiveSupport::Dependencies.autoload_paths.unshift controller_path
@@ -6,6 +8,11 @@ Rails.configuration.to_prepare do
   view_path = File.dirname(__FILE__)+'/app/views'
   ::ApplicationController.view_paths.delete view_path
   ::ApplicationController.view_paths.unshift view_path
+
+  Permissions.register :view_analytics,
+    :label => lambda { I18n.t('#role_override.permissions.view_analytics', "View analytics pages") },
+    :available_to => %w(TaEnrollment TeacherEnrollment AccountAdmin AccountMembership),
+    :true_for => %w(AccountAdmin)
 
   require_dependency 'analytics/extensions'
 end
