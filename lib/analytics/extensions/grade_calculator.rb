395919b8e4288c30caad538b74f429de2aacef13
Cameron Matheson <cameron@instructure.com>
2012-12-18 18:16:22 -0700

    update method names in extensions/grade_calculator.rb
    Iebe15c5d108dcfa899bd501cc6dd45ceb688fa25 splits
    GradeCalculator.recompute_and_save_scores into .compute_scores and
    .save_scores.  The behavior is otherwise the same.
    
    Change-Id: I6e74a8c9646efb7659e87769fc68f5099ddb73c8
    Reviewed-on: https://gerrit.instructure.com/16202
    Reviewed-by: Simon Williams <simon@instructure.com>
    QA-Review: Simon Williams <simon@instructure.com>
    Tested-by: Simon Williams <simon@instructure.com>

--- lib/analytics/extensions/grade_calculator.rb
+++ lib/analytics/extensions/grade_calculator.rb
@@ -1,11 +1,11 @@
 GradeCalculator.class_eval do
   # after recomputing current scores on enrollments in the course, recache its
   # grade distribution
-  def recompute_and_save_scores_with_cached_grade_distribution
-    recompute_and_save_scores_without_cached_grade_distribution
+  def save_scores_with_cached_grade_distribution
+    save_scores_without_cached_grade_distribution
     unless @current_updates.empty? && @final_updates.empty?
       @course.recache_grade_distribution
     end
   end
-  alias_method_chain :recompute_and_save_scores, :cached_grade_distribution
+  alias_method_chain :save_scores, :cached_grade_distribution
 end
