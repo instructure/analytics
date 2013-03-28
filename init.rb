3f1a405484a2f26cadcc33f4369dba2046c05ec6
Cody Cutrer <cody@instructure.com>
2013-03-28 14:22:00 -0600

    boot in Rails 3
    Change-Id: I3f38cbb92f68afb9ae4108bf942fc614c3ab07d3
    Reviewed-on: https://gerrit.instructure.com/19087
    Tested-by: Jenkins <jenkins@instructure.com>
    Reviewed-by: Brian Palmer <brianp@instructure.com>
    Reviewed-by: Jacob Fugal <jacob@instructure.com>
    Product-Review: Cody Cutrer <cody@instructure.com>
    QA-Review: Cody Cutrer <cody@instructure.com>

--- init.rb
+++ init.rb
@@ -4,10 +4,11 @@ controller_path = File.dirname(__FILE__)+'/app/controllers'
 ActiveSupport::Dependencies.autoload_paths.delete controller_path
 ActiveSupport::Dependencies.autoload_paths.unshift controller_path
 
+
 Rails.configuration.to_prepare do
   view_path = File.dirname(__FILE__)+'/app/views'
   ::ApplicationController.view_paths.delete view_path
-  ::ApplicationController.view_paths.unshift view_path
+  ::ApplicationController.prepend_view_path(view_path)
 
   Account.register_service :analytics,
     :name => "Analytics",
