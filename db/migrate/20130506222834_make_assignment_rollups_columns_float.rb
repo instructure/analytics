2624f9b7fa40aba2d3cbbaa167f0b37d9d67c52e
Brian Palmer <brianp@instructure.com>
2013-05-06 16:32:29 -0600

    change assignment_rollups columns to float where appropriate
    Some columns were created as integer when they should've been float,
    which won't work since assignments.points_possible is a float field not
    an integer field
    
    refs CNVS-5720
    
    Change-Id: I8bad753e7f47af451099834a5007439499c4382c
    Reviewed-on: https://gerrit.instructure.com/20403
    Tested-by: Jenkins <jenkins@instructure.com>
    Reviewed-by: Ethan Vizitei <ethan@12spokes.com>
    Product-Review: Brian Palmer <brianp@instructure.com>
    QA-Review: Brian Palmer <brianp@instructure.com>

---
+++ db/migrate/20130506222834_make_assignment_rollups_columns_float.rb
@@ -0,0 +1,17 @@
+class MakeAssignmentRollupsColumnsFloat < ActiveRecord::Migration
+  tag :predeploy
+
+  def self.up
+    change_column :assignment_rollups, :max_score, :float
+    change_column :assignment_rollups, :median_score, :float
+    change_column :assignment_rollups, :min_score, :float
+    change_column :assignment_rollups, :points_possible, :float
+  end
+
+  def self.down
+    change_column :assignment_rollups, :max_score, :integer
+    change_column :assignment_rollups, :median_score, :integer
+    change_column :assignment_rollups, :min_score, :integer
+    change_column :assignment_rollups, :points_possible, :integer
+  end
+end
