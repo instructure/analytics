c57d9bce82c10e0c4ff1431b3acae6505822bfaf
Jacob Fugal <jacob@instructure.com>
2013-07-16 11:12:41 -0600

    use all section rollups with full visibility
    when building assignment data for course-level analytics, we combine the
    visible section-level rollups. but it's possible for a user to have
    :full visibility level without having explicit visibility to each
    section (e.g. admin with no enrollments, or unrestricted teacher that
    happens to only have one section enrollment). in those cases, the
    rollups from all active sections should still be used.
    
    fixes CNVS-6918
    
    test-plan:
     - view course level analytics as an admin that has no enrollments in
       the course
     - should see assignment and grade analytics
    
    Change-Id: I64db22590c7c2ee1a21bd4f4a7f250de3a685cb3
    Reviewed-on: https://gerrit.instructure.com/22372
    Reviewed-by: Cody Cutrer <cody@instructure.com>
    Reviewed-by: Brian Palmer <brianp@instructure.com>
    Tested-by: Jenkins <jenkins@instructure.com>
    QA-Review: Jeremy Putnam <jeremyp@instructure.com>
    Product-Review: Jacob Fugal <jacob@instructure.com>
    
    Conflicts:
    	lib/analytics/permitted_course.rb
    	spec_canvas/lib/analytics/permitted_course_spec.rb

--- spec_canvas/lib/analytics/permitted_course_spec.rb
+++ spec_canvas/lib/analytics/permitted_course_spec.rb
@@ -3,15 +3,21 @@ module Analytics
   describe PermittedCourse do
     describe '#assignments' do
       let(:analytics) do
-        stub('course_analytics', {
-          :assignment_rollups_for => ['SECTIONAL_ROLLUP'],
-          :assignments => ['ASSIGNMENT_DATA']
-        })
+        stub('course_analytics',
+          assignment_rollups_for: ['SECTIONAL_ROLLUP'],
+          assignments: ['ASSIGNMENT_DATA'])
       end
 
       let(:user) { stub('user') }
       let(:shard) { stub('shard') }
-      let(:course) { stub('course', :shard => shard, :section_visibilities_for => []) }
+      let(:course) do
+        stub('course',
+          shard: shard,
+          section_visibilities_for: [{course_section_id: 'SECTION_ID1'}],
+          course_sections: stub('course_sections',
+            active: stub('active_course_sections',
+              pluck: ['SECTION_ID1', 'SECTION_ID2'])))
+      end
       let(:permitted_course) { PermittedCourse.new(user, course, analytics) }
 
       before do
@@ -26,6 +32,18 @@ module Analytics
       it 'uses tallied rollups for section visibility' do
         course.stubs(:enrollment_visibility_level_for).returns(:sections)
         permitted_course.assignments.should == ['SECTIONAL_ROLLUP']
+      end
+
+      it 'includes all sections for full visibility users regardless of enrollments' do
+        course.stubs(:enrollment_visibility_level_for).returns(:full)
+        analytics.expects(:assignment_rollups_for).with(['SECTION_ID1', 'SECTION_ID2'])
+        permitted_course.assignments_uncached
+      end
+
+      it 'limits to visible sections for section visibility users' do
+        course.stubs(:enrollment_visibility_level_for).returns(:sections)
+        analytics.expects(:assignment_rollups_for).with(['SECTION_ID1'])
+        permitted_course.assignments_uncached
       end
 
       it 'tallys assignments for direct visibility' do
