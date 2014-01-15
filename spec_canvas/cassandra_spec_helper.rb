52ed469433f7631db912bc7e0c985490296ba375
Brian Palmer <brianp@instructure.com>
2014-01-15 13:55:30 -0700

    handle refactored Cassandra::DatabaseBuilder
    Change-Id: I9798636f371646e10ea9b581e1f9cf455d271bc3
    Signed-off-by: Stephan Hagemann <stephan.hagemann@instructure.com>
    Reviewed-on: https://gerrit.instructure.com/28629
    Product-Review: Brian Palmer <brianp@instructure.com>
    Tested-by: Brian Palmer <brianp@instructure.com>
    Reviewed-by: Brian Palmer <brianp@instructure.com>
    QA-Review: Brian Palmer <brianp@instructure.com>

--- spec_canvas/cassandra_spec_helper.rb
+++ spec_canvas/cassandra_spec_helper.rb
@@ -3,7 +3,7 @@ require File.expand_path(File.dirname(__FILE__) + '/../../../../spec/cassandra_s
 shared_examples_for "analytics cassandra page views" do
   it_should_behave_like "cassandra page views"
   before do
-    if Canvas::Cassandra::Database.configured?('page_views')
+    if Canvas::Cassandra::DatabaseBuilder.configured?('page_views')
       PageView::EventStream.database.execute("TRUNCATE page_views_counters_by_context_and_user")
       PageView::EventStream.database.execute("TRUNCATE page_views_counters_by_context_and_hour")
       PageView::EventStream.database.execute("TRUNCATE participations_by_context")
