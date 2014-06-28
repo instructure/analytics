77e366ab3b54a94825b5b342aa7619b1aeb414e6
Rob Orton <rob@instructure.com>
2014-06-27 20:25:43 -0600

    add copyright to prepare for open source
    refs CNVS-13519
    
    test plan
     - these are just comments duh
    
    Change-Id: I4647140147239eee55306ed822440bee4392b9c1
    Reviewed-on: https://gerrit.instructure.com/37037
    Tested-by: Jenkins <jenkins@instructure.com>
    QA-Review: August Thornton <august@instructure.com>
    Reviewed-by: Brian Palmer <brianp@instructure.com>
    Product-Review: Rob Orton <rob@instructure.com>

--- app/models/page_views_rollup.rb
+++ app/models/page_views_rollup.rb
@@ -1,3 +1,21 @@
+#
+# Copyright (C) 2014 Instructure, Inc.
+#
+# This file is part of Canvas.
+#
+# Canvas is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, version 3 of the License.
+#
+# Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
 # while the rows will only ever be queried with respect courses that are
 # available or completed course, we don't know at the time of rollup what the
 # future workflow state of course will be. so we have to keep rollup data for
