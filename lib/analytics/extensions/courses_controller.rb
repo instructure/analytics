c28448c39e7cb95c703aed5dfc8a2113d3b18466
Ryan Shaw <ryan@instructure.com>
2015-03-24 14:49:15 -0600

    s/jammit_css/css_bundle for analytics
    now that the themeditor commit is in master, jammit_css has been
    deprecated in favor of css_bundle. 
    
    test plan:
    
    checkout commit, 
    compile assets, 
    go to a couple analytics pages and make sure they work
    
    Change-Id: I23fc796b83c62a0d93f7aa3e238935b3d675d95c
    Reviewed-on: https://gerrit.instructure.com/50923
    Tested-by: Jenkins
    Reviewed-by: Jacob Fugal <jacob@instructure.com>
    Product-Review: Ryan Shaw <ryan@instructure.com>
    QA-Review: Ryan Shaw <ryan@instructure.com>

--- lib/analytics/extensions/courses_controller.rb
+++ lib/analytics/extensions/courses_controller.rb
@@ -29,7 +29,7 @@ CoursesController.class_eval do
       # inject a button to the analytics page for the course
       js_env :ANALYTICS => { 'link' => analytics_course_path(:course_id => @context.id) }
       js_bundle :inject_course_analytics, :plugin => :analytics
-      jammit_css :analytics_buttons, :plugin => :analytics
+      css_bundle :analytics_buttons, :plugin => :analytics
     end
 
     # continue rendering the page
