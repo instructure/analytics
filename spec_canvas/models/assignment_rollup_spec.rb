55a05634d661b7122e8b8431c3877bcda782de47
Jacob Fugal <jacob@instructure.com>
2015-10-20 15:03:55 -0600

    use full FakeSubmission for assignment rollup calc
    fixes CNVS-23685
    
    test-plan:
     - create an external tool assignment with a due date in the past
     - view course analytics for that course
     - assignment data should load
    
    Change-Id: Id2ccaadc1a8e247271e4f7b87ee4e887d5884263
    Reviewed-on: https://gerrit.instructure.com/65481
    Tested-by: Jenkins
    Reviewed-by: Rob Orton <rob@instructure.com>
    QA-Review: August Thornton <august@instructure.com>
    Product-Review: Jacob Fugal <jacob@instructure.com>

---
+++ spec_canvas/models/assignment_rollup_spec.rb
@@ -0,0 +1,31 @@
+#
+# Copyright (C) 2015 Instructure, Inc.
+#
+# This file is part of Canvas.
+#
+# Canvas is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, version 3 of the License.
+#
+# Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+require_relative '../../../../../spec/spec_helper'
+
+describe AssignmentRollup do
+  describe "build" do
+    it "should work with a missing external tool assignment" do
+      assignment_model(due_at: 1.day.ago, submission_types: "external_tool")
+      submission_model(assignment: @assignment, submitted_at: nil)
+      rollup = AssignmentRollup.build(@course, @assignment)
+      rollup = rollup[@course.default_section.id]
+      expect(rollup.missing_submissions).to eql(1.0)
+    end
+  end
+end
