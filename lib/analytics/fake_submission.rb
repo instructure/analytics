6059bfb1838ec76c2beb845b7c7e6652a3cee6ab
Spencer Olson <solson@instructure.com>
2017-08-05 09:53:09 -0500

    alias excused to excused? for fake submissions
    The excused? method can now be safely called on a fake submission.
    
    Change-Id: Ia8053a7967bb37390d45c8ac258e877fb90bef8e
    Reviewed-on: https://gerrit.instructure.com/121698
    Reviewed-by: Keith T. Garner <kgarner@instructure.com>
    Reviewed-by: Jeremy Neander <jneander@instructure.com>
    Tested-by: Jenkins
    QA-Review: Neil Gupta <ngupta@instructure.com>
    Product-Review: Keith T. Garner <kgarner@instructure.com>

--- lib/analytics/fake_submission.rb
+++ lib/analytics/fake_submission.rb
@@ -67,6 +67,8 @@ module Analytics
       @seconds_late_override = data['seconds_late_override']
     end
 
+    alias_method :excused?, :excused
+
     def self.from_scope(scope)
       ActiveRecord::Base.connection.select_all(scope).map{ |data| self.new(data) }
     end
