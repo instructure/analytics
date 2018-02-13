f75b5d4340ad0187ca422489d696682f1a7f09b4
James Williams <jamesw@instructure.com>
2018-02-13 13:17:15 -0700

    spec: use built-in roles directly for manage_role_override
    Change-Id: Id622e2a7b704b04faeede58828fd666fabcb5822
    Reviewed-on: https://gerrit.instructure.com/140815
    Tested-by: Jenkins
    Reviewed-by: Simon Williams <simon@instructure.com>
    Product-Review: James Williams  <jamesw@instructure.com>
    QA-Review: James Williams  <jamesw@instructure.com>

--- spec_canvas/graphql/user_type_spec.rb
+++ spec_canvas/graphql/user_type_spec.rb
@@ -33,7 +33,7 @@ describe Types::UserType do
 
     it "is nil for teachers without permission" do
       RoleOverride.manage_role_override(
-        Account.default, 'TeacherEnrollment', 'view_analytics',
+        Account.default, teacher_role, 'view_analytics',
         override: false
       )
       expect(
