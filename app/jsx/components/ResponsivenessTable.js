0b3d03e1248c21cb5d5ed39da12ab6c016709216
Ryan Shaw <ryankshaw@gmail.com>
2018-09-08 22:50:15 -0600

    amdtoes6 app/jsx
    Change-Id: I449250e0bbb64f933410c57cd6b6c9969a135e03
    Reviewed-on: https://gerrit.instructure.com/163716
    Tested-by: Jenkins
    Reviewed-by: Clay Diffrient <cdiffrient@instructure.com>
    Product-Review: Ryan Shaw <ryan@instructure.com>
    QA-Review: Ryan Shaw <ryan@instructure.com>

--- app/jsx/components/ResponsivenessTable.js
+++ app/jsx/components/ResponsivenessTable.js
@@ -1,46 +1,44 @@
-define([
-  'react',
-  'plugins/analytics/react-bootstrap-table',
-  'i18n!analytics',
-  'analytics/compiled/helpers'
-], function (React, ReactBootstrapTable, I18n, helpers) {
+import React from 'react'
+import ReactBootstrapTable from 'plugins/analytics/react-bootstrap-table'
+import I18n from 'i18n!analytics'
+import helpers from 'analytics/compiled/helpers'
 
-  const { BootstrapTable, TableHeaderColumn } = ReactBootstrapTable;
+const {BootstrapTable, TableHeaderColumn} = ReactBootstrapTable
 
-  const tableOptions = {
-    sizePerPage: 30,
-    sizePerPageList: []
-  };
+const tableOptions = {
+  sizePerPage: 30,
+  sizePerPageList: []
+}
 
-  return React.createClass({
-    displayName: 'ResponsivenessTable',
+export default class ResponsivenessTable extends React.Component {
+  static propTypes = {
+    data: React.PropTypes.object.isRequired
+  }
 
-    propTypes: {
-      data: React.PropTypes.object.isRequired
-    },
+  formatDate = (cell, row) => I18n.l('date.formats.default', cell)
 
-    formatDate (cell, row) {
-        return I18n.l("date.formats.default", cell);
-    },
-
-    formatNumber (styles = {}) {
-      styles.fontWeight = 'bold';
-      return function (cell, row) {
-        return <span style={styles}>{helpers.formatNumber(cell)}</span>;
-      };
-    },
-
-    render () {
-      return (
-        <div>
-          <BootstrapTable data={this.props.data} pagination={true} options={tableOptions}>
-            <TableHeaderColumn dataField="date" isKey={true} dataFormat={this.formatDate}>{I18n.t("Date")}</TableHeaderColumn>
-            <TableHeaderColumn dataField="instructorMessages" dataFormat={this.formatNumber()}>{I18n.t("Instructor Messages")}</TableHeaderColumn>
-            <TableHeaderColumn dataField="studentMessages" dataFormat={this.formatNumber()}>{I18n.t("Student Messages")}</TableHeaderColumn>
-          </BootstrapTable>
-        </div>
-
-      );
+  formatNumber = (styles = {}) => {
+    styles.fontWeight = 'bold'
+    return function(cell, row) {
+      return <span style={styles}>{helpers.formatNumber(cell)}</span>
     }
-  });
-});
+  }
+
+  render() {
+    return (
+      <div>
+        <BootstrapTable data={this.props.data} pagination options={tableOptions}>
+          <TableHeaderColumn dataField="date" isKey dataFormat={this.formatDate}>
+            {I18n.t('Date')}
+          </TableHeaderColumn>
+          <TableHeaderColumn dataField="instructorMessages" dataFormat={this.formatNumber()}>
+            {I18n.t('Instructor Messages')}
+          </TableHeaderColumn>
+          <TableHeaderColumn dataField="studentMessages" dataFormat={this.formatNumber()}>
+            {I18n.t('Student Messages')}
+          </TableHeaderColumn>
+        </BootstrapTable>
+      </div>
+    )
+  }
+}
--- app/jsx/components/SubmissionsTable.js
+++ app/jsx/components/SubmissionsTable.js
@@ -1,43 +1,44 @@
-define([
-    'react',
-    'plugins/analytics/react-bootstrap-table',
-    'i18n!analytics'
-], function (React, ReactBootstrapTable, I18n) {
+import React from 'react'
+import ReactBootstrapTable from 'plugins/analytics/react-bootstrap-table'
+import I18n from 'i18n!analytics'
 
-    const { BootstrapTable, TableHeaderColumn } = ReactBootstrapTable;
+const {BootstrapTable, TableHeaderColumn} = ReactBootstrapTable
 
-    const tableOptions = {
-        sizePerPage: 30,
-        sizePerPageList: []
-    };
+const tableOptions = {
+  sizePerPage: 30,
+  sizePerPageList: []
+}
 
-    return React.createClass({
-        displayName: 'SubmissionsTable',
+export default class SubmissionsTable extends React.Component {
+  static propTypes = {
+    data: React.PropTypes.object.isRequired
+  }
 
-        propTypes: {
-            data: React.PropTypes.object.isRequired
-        },
+  formatPercentStyle = (styles = {}) => {
+    styles.fontWeight = 'bold'
+    return function(cell, row) {
+      return <span style={styles}>{Math.round(Math.floor(Number.parseFloat(cell) * 100))}%</span>
+    }
+  }
 
-        formatPercentStyle (styles = {}) {
-            styles.fontWeight = 'bold';
-            return function (cell, row) {
-                return <span style={styles}>{Math.round(Math.floor(Number.parseFloat(cell) * 100))}%</span>;
-            }
-        },
-
-        render () {
-            return (
-                <div>
-                    <BootstrapTable data={this.props.data} pagination={true} options={tableOptions}>
-                        <TableHeaderColumn dataField="title" isKey={true}>{I18n.t("Assignment")}</TableHeaderColumn>
-                        <TableHeaderColumn dataField="missing" dataFormat={this.formatPercentStyle()}>{I18n.t("Missing")}</TableHeaderColumn>
-                        <TableHeaderColumn dataField="late" dataFormat={this.formatPercentStyle()}>{I18n.t("Late")}</TableHeaderColumn>
-                        <TableHeaderColumn dataField="onTime" dataFormat={this.formatPercentStyle()}>{I18n.t("On Time")}</TableHeaderColumn>
-                    </BootstrapTable>
-                </div>
-
-            );
-        }
-    });
-});
-
+  render() {
+    return (
+      <div>
+        <BootstrapTable data={this.props.data} pagination options={tableOptions}>
+          <TableHeaderColumn dataField="title" isKey>
+            {I18n.t('Assignment')}
+          </TableHeaderColumn>
+          <TableHeaderColumn dataField="missing" dataFormat={this.formatPercentStyle()}>
+            {I18n.t('Missing')}
+          </TableHeaderColumn>
+          <TableHeaderColumn dataField="late" dataFormat={this.formatPercentStyle()}>
+            {I18n.t('Late')}
+          </TableHeaderColumn>
+          <TableHeaderColumn dataField="onTime" dataFormat={this.formatPercentStyle()}>
+            {I18n.t('On Time')}
+          </TableHeaderColumn>
+        </BootstrapTable>
+      </div>
+    )
+  }
+}
