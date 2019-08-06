cde20171dae65a724d4acb96dd927778a68f88b6
Ryan Shaw <ryan@instructure.com>
2019-08-06 12:20:53 -0600

    Remove a “trick babel” hack in analytics
    Test plan
    * with or without the “Drop IE 11” commit applied
    * bin/rspec ./gems/plugins/analytics/spec_canvas/selenium/analytics_account_view_spec.rb:71
      Should pass
    
    Or to test manually:
    * apply the “drop ie11” commit in canvas-lms
    * check out this commit in gems/plugins/analytics
    * go to /accounts/x/analytics
    * it should load successfully and not die on this error at page load:
      “Uncaught TypeError: Cannot read property '0' of null”
    
    Change-Id: Ic402e444e3309028157c12576648c6afdd31cb34
    Reviewed-on: https://gerrit.instructure.com/204050
    Tested-by: Jenkins
    Reviewed-by: Steven Burnett <sburnett@instructure.com>
    QA-Review: Steven Burnett <sburnett@instructure.com>
    Product-Review: Ryan Shaw <ryan@instructure.com>

--- app/jsx/Department/DepartmentFilterBox.js
+++ app/jsx/Department/DepartmentFilterBox.js
@@ -3,26 +3,17 @@ import DepartmentRouter from '../Department/DepartmentRouter'
 
 export default class DepartmentFilterBox extends ComboBox {
   constructor(model) {
-
-    {
-      // Hack: trick Babel/TypeScript into allowing this before super.
-      if (false) { super(); }
-      let thisFn = (() => { return this; }).toString();
-      let thisName = thisFn.match(/_this\d*/)[0];
-      eval(`${thisName} = this;`);
-    }
-
+    // construct combobox
+    super(model.get('filters').models, {
+      value: filter => filter.get('id'),
+      label: filter => filter.get('label'),
+      selected: model.get('filter').get('id')
+    })
     this.model = model
 
     // add a router tied to the model
     this.router = new DepartmentRouter(this.model)
 
-    // construct combobox
-    super(this.model.get('filters').models, {
-      value: filter => filter.get('id'),
-      label: filter => filter.get('label'),
-      selected: this.model.get('filter').get('id')
-    })
 
     // connect combobox to model
     this.on('change', this.push)
