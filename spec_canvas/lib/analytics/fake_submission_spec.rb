87e16aa4f40bf1e2db93aa3acfd7ccde8394b3cb
James Butters <jbutters@instructure.com>
2020-07-30 07:55:02 -0600

    fixup fake_submission_spec.rb to follow spec standards
    fake_submissions_spec.rb currently doensn't follow spec
    standards followed in other analytics spec files. Add module,
    require_dependency, and require canvas-lms spec_helper
    
    Change-Id: I964153333239d16721f85f4d1b3ec92f518c2404
    Reviewed-on: https://gerrit.instructure.com/c/analytics/+/243847
    Reviewed-by: Aaron Ogata <aogata@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Tested-by: James Butters <jbutters@instructure.com>
    QA-Review: James Butters <jbutters@instructure.com>
    Product-Review: James Butters <jbutters@instructure.com>

--- spec_canvas/lib/analytics/fake_submission_spec.rb
+++ spec_canvas/lib/analytics/fake_submission_spec.rb
@@ -17,127 +17,131 @@
 #
 
 require 'spec_helper'
+require_relative '../../../../../../spec/spec_helper'
+require_dependency 'analytics/fake_submission'
 
-describe Analytics::FakeSubmission do
-  before(:once) do
-    @course = Course.create!
-    @student = @course.enroll_student(User.create!, enrollment_state: :active)
-    @assignment = @course.assignments.create!(submission_types: "online_text_entry")
-  end
-
-  let(:late_fake_submission) do
-    now = Time.zone.now
-    due_at = 2.hours.ago(now)
-    submission = Analytics::FakeSubmission.new(
-      "assignment_id" => @assignment.id,
-      "user_id" => @student.id,
-      "cached_due_date" => due_at,
-      "submission_type" => "online_upload",
-      "submitted_at" => now
-    )
-    submission.assignment = @assignment
-    submission
-  end
-
-  let(:on_time_fake_submission) do
-    due_at = 2.hours.ago
-    submission = Analytics::FakeSubmission.new(
-      "assignment_id" => @assignment.id,
-      "user_id" => @student.id,
-      "cached_due_date" => due_at,
-      "submission_type" => "online_upload",
-      "submitted_at" => due_at
-    )
-    submission.assignment = @assignment
-    submission
-  end
-
-  it ".from_scope creates a FakeSubmission from each submission returned by the scope" do
-    submissions_from_scope = Analytics::FakeSubmission.from_scope(@course.submissions)
-    expect(submissions_from_scope.first).to be_a Analytics::FakeSubmission
-  end
-
-  describe "#excused?" do
-    it "returns true if the submission is excused" do
-      submission = Analytics::FakeSubmission.new("excused" => true)
-      expect(submission).to be_excused
+module Analytics
+  describe FakeSubmission do
+    before(:once) do
+      @course = Course.create!
+      @student = @course.enroll_student(User.create!, enrollment_state: :active)
+      @assignment = @course.assignments.create!(submission_types: "online_text_entry")
     end
 
-    it "returns false if the submission is not excused" do
-      submission = Analytics::FakeSubmission.new("excused" => false)
-      expect(submission).not_to be_excused
-    end
-  end
-
-  describe "#graded?" do
-    it "returns true if the submission has a score and workflow state is 'graded'" do
-      submission = Analytics::FakeSubmission.new("score" => 5.0, "workflow_state" => "graded")
-      expect(submission).to be_graded
-    end
-
-    it "returns true if the submission is excused" do
-      submission = Analytics::FakeSubmission.new("excused" => true)
-      expect(submission).to be_graded
-    end
-
-    it "returns false if the submission is not graded" do
-      submission = Analytics::FakeSubmission.new({})
-      expect(submission).not_to be_graded
-    end
-  end
-
-  describe "#late?" do
-    it "returns true if submitted and past due" do
-      expect(late_fake_submission).to be_late
-    end
-
-    it "returns false if submitted on time" do
-      expect(on_time_fake_submission).not_to be_late
-    end
-  end
-
-  describe "#past_due?" do
-    it "returns true if submitted and past due" do
-      expect(late_fake_submission).to be_past_due
-    end
-
-    it "returns false if submitted on time" do
-      expect(on_time_fake_submission).not_to be_past_due
-    end
-  end
-
-  describe "#seconds_late" do
-    it "returns the number of seconds late a submission is" do
-      expect(late_fake_submission.seconds_late).to be 7200
-    end
-
-    it "returns the overridden seconds late if late_policy_status is 'late'" do
-      submission = Analytics::FakeSubmission.new("late_policy_status" => "late", "seconds_late_override" => 25)
-      expect(submission.seconds_late).to be 25
-    end
-  end
-
-  describe "#missing?" do
-    it "returns true if not submitted and past due" do
-      due_at = 3.weeks.ago
+    let(:late_fake_submission) do
+      now = Time.zone.now
+      due_at = 2.hours.ago(now)
       submission = Analytics::FakeSubmission.new(
-        "assignment_id" => @assignment.id,
-        "user_id" => @student.id,
-        "cached_due_date" => due_at,
-        "submission_type" => "online_upload"
+          "assignment_id" => @assignment.id,
+          "user_id" => @student.id,
+          "cached_due_date" => due_at,
+          "submission_type" => "online_upload",
+          "submitted_at" => now
       )
       submission.assignment = @assignment
-
-      expect(submission).to be_missing
+      submission
     end
 
-    it "returns false if submitted and past due" do
-      expect(late_fake_submission).not_to be_missing
+    let(:on_time_fake_submission) do
+      due_at = 2.hours.ago
+      submission = Analytics::FakeSubmission.new(
+          "assignment_id" => @assignment.id,
+          "user_id" => @student.id,
+          "cached_due_date" => due_at,
+          "submission_type" => "online_upload",
+          "submitted_at" => due_at
+      )
+      submission.assignment = @assignment
+      submission
     end
-  end
 
-  it "sets submitted_at to nil if there is no submission_type" do
-    submission = Analytics::FakeSubmission.new("submitted_at" => 2.days.ago)
-    expect(submission.submitted_at).to be_nil
+    it ".from_scope creates a FakeSubmission from each submission returned by the scope" do
+      submissions_from_scope = Analytics::FakeSubmission.from_scope(@course.submissions)
+      expect(submissions_from_scope.first).to be_a Analytics::FakeSubmission
+    end
+
+    describe "#excused?" do
+      it "returns true if the submission is excused" do
+        submission = Analytics::FakeSubmission.new("excused" => true)
+        expect(submission).to be_excused
+      end
+
+      it "returns false if the submission is not excused" do
+        submission = Analytics::FakeSubmission.new("excused" => false)
+        expect(submission).not_to be_excused
+      end
+    end
+
+    describe "#graded?" do
+      it "returns true if the submission has a score and workflow state is 'graded'" do
+        submission = Analytics::FakeSubmission.new("score" => 5.0, "workflow_state" => "graded")
+        expect(submission).to be_graded
+      end
+
+      it "returns true if the submission is excused" do
+        submission = Analytics::FakeSubmission.new("excused" => true)
+        expect(submission).to be_graded
+      end
+
+      it "returns false if the submission is not graded" do
+        submission = Analytics::FakeSubmission.new({})
+        expect(submission).not_to be_graded
+      end
+    end
+
+    describe "#late?" do
+      it "returns true if submitted and past due" do
+        expect(late_fake_submission).to be_late
+      end
+
+      it "returns false if submitted on time" do
+        expect(on_time_fake_submission).not_to be_late
+      end
+    end
+
+    describe "#past_due?" do
+      it "returns true if submitted and past due" do
+        expect(late_fake_submission).to be_past_due
+      end
+
+      it "returns false if submitted on time" do
+        expect(on_time_fake_submission).not_to be_past_due
+      end
+    end
+
+    describe "#seconds_late" do
+      it "returns the number of seconds late a submission is" do
+        expect(late_fake_submission.seconds_late).to be 7200
+      end
+
+      it "returns the overridden seconds late if late_policy_status is 'late'" do
+        submission = Analytics::FakeSubmission.new("late_policy_status" => "late", "seconds_late_override" => 25)
+        expect(submission.seconds_late).to be 25
+      end
+    end
+
+    describe "#missing?" do
+      it "returns true if not submitted and past due" do
+        due_at = 3.weeks.ago
+        submission = Analytics::FakeSubmission.new(
+            "assignment_id" => @assignment.id,
+            "user_id" => @student.id,
+            "cached_due_date" => due_at,
+            "submission_type" => "online_upload"
+        )
+        submission.assignment = @assignment
+
+        expect(submission).to be_missing
+      end
+
+      it "returns false if submitted and past due" do
+        expect(late_fake_submission).not_to be_missing
+      end
+    end
+
+    it "sets submitted_at to nil if there is no submission_type" do
+      submission = Analytics::FakeSubmission.new("submitted_at" => 2.days.ago)
+      expect(submission.submitted_at).to be_nil
+    end
   end
 end
