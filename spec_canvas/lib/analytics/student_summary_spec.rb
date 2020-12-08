b438125e44a86f46cf151162e21f571b98bdab7f
Ethan Vizitei <evizitei@instructure.com>
2020-12-08 13:52:09 -0600

    don't explode if we have no breakdown for a student
    closes FOO-1288
    
    Change-Id: I35f8c2df1f1fa228fed66f0e87c64f92e568ba49
    Reviewed-on: https://gerrit.instructure.com/c/analytics/+/254643
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Cody Cutrer <cody@instructure.com>
    Product-Review: Cody Cutrer <cody@instructure.com>
    QA-Review: Ethan Vizitei <evizitei@instructure.com>

---
+++ spec_canvas/lib/analytics/student_summary_spec.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+#
+# Copyright (C) 2014 Instructure, Inc.
+#
+# This file is part of Canvas.
+#
+# Canvas is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, version 3 of the License.
+#
+# Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+require_relative '../../../../../../spec/spec_helper'
+require_dependency "analytics/student_summary"
+
+module Analytics
+  describe StudentSummary do
+    describe "#tardiness_breakdown" do
+      it "serves a blank hash for a missing student id" do
+        student = double(id: 42)
+        course = double(tardiness_breakdowns: { students: {} })
+        page_view_counts = double()
+        analysis = double()
+        summary = StudentSummary.new(student, course, page_view_counts, analysis)
+        expect(summary.tardiness_breakdown).to eq({})
+      end
+    end
+  end
+end
