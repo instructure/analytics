d5df655f7906effa5ae7e326afaa66bb68d1d577
Kai Bjorkman <kbjorkman@instructure.com>
2021-11-02 11:12:11 -0600

    submission object tweaks for remove_missing_status_when_graded FF
    flag=remove_missing_status_when_graded
    closes EVAL-2081
    
    Test Plan:
        -specs pass
    
    Change-Id: I8532fe5434518c2c71709a80fa0f14f53157f10d
    Reviewed-on: https://gerrit.instructure.com/c/analytics/+/277274
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Dustin Cowles <dustin.cowles@instructure.com>
    Reviewed-by: Spencer Olson <solson@instructure.com>
    QA-Review: Kai Bjorkman <kbjorkman@instructure.com>
    Product-Review: Syed Hussain <shussain@instructure.com>

--- lib/analytics/fake_submission.rb
+++ lib/analytics/fake_submission.rb
@@ -31,8 +31,8 @@ module Analytics
     attr_accessor :assignment
     attr_reader   :assignment_id, :user_id, :score, :submission_type,
                   :workflow_state, :excused, :submitted_at, :cached_due_date,
-                  :graded_at, :late_policy_status, :accepted_at, :seconds_late_override,
-                  :cached_quiz_lti
+                  :graded_at, :grader_id, :late_policy_status, :accepted_at,
+                  :seconds_late_override, :cached_quiz_lti
 
     alias_method :excused?, :excused
     alias_method :cached_quiz_lti?, :cached_quiz_lti
@@ -47,6 +47,7 @@ module Analytics
       @submission_type = data['submission_type']
       @workflow_state  = data['workflow_state']
       @graded_at       = data['graded_at']
+      @grader_id       = data['grader_id']
       @cached_due_date = data['cached_due_date']
       @late_policy_status = data['late_policy_status']
       @cached_quiz_lti = data['cached_quiz_lti'].present? ? data['cached_quiz_lti'] : false
