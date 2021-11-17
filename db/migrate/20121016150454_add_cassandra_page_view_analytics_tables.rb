96182299f9e81c55ac8202be4868d89d13872aa2
Cody Cutrer <cody@instructure.com>
2021-11-17 14:33:16 -0700

    RuboCop: Style/PercentLiteralDelimiters
    auto-corrected, with post-review changing some multiline strings to
    heredocs
    
    Change-Id: I5a505746875afd85f3cdddfd43a09fa0b1df5ab9
    Reviewed-on: https://gerrit.instructure.com/c/analytics/+/278654
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Jacob Burroughs <jburroughs@instructure.com>
    QA-Review: Cody Cutrer <cody@instructure.com>
    Product-Review: Cody Cutrer <cody@instructure.com>
    Migration-Review: Cody Cutrer <cody@instructure.com>

--- db/migrate/20121016150454_add_cassandra_page_view_analytics_tables.rb
+++ db/migrate/20121016150454_add_cassandra_page_view_analytics_tables.rb
@@ -14,7 +14,7 @@ class AddCassandraPageViewAnalyticsTables < ActiveRecord::Migration[4.2]
       "WITH compression = { 'sstable_compression' : 'DeflateCompressor' }" :
       "WITH compression_parameters:sstable_compression='DeflateCompressor'"
 
-    cassandra.execute %{
+    cassandra.execute <<~SQL.squish
       CREATE TABLE participations_by_context (
         context text,
         created_at timestamp,
@@ -24,34 +24,37 @@ class AddCassandraPageViewAnalyticsTables < ActiveRecord::Migration[4.2]
         asset_user_access_id text,
         url text,
         PRIMARY KEY (context, created_at, request_id)
-      ) #{compression_params}}
+      ) #{compression_params}
+    SQL
 
-    cassandra.execute %{
+    cassandra.execute <<~SQL.squish
       CREATE TABLE page_views_counters_by_context_and_hour (
         context text,
         hour_bucket int,
         page_view_count counter,
         PRIMARY KEY (context, hour_bucket)
-      ) #{compression_params}}
+      ) #{compression_params}
+    SQL
 
-    cassandra.execute %{
+    cassandra.execute <<~SQL.squish
       CREATE TABLE page_views_counters_by_context_and_user (
         context text,
         user_id text,
         page_view_count counter,
         PRIMARY KEY (context, user_id)
-      ) #{compression_params}}
+      ) #{compression_params}
+    SQL
   end
 
   def self.down
-    cassandra.execute %{
+    cassandra.execute <<~SQL.squish
       DROP TABLE participations_by_context;
-    }
-    cassandra.execute %{
+    SQL
+    cassandra.execute <<~SQL.squish
       DROP TABLE page_views_counters_by_context_and_hour;
-    }
-    cassandra.execute %{
+    SQL
+    cassandra.execute <<~SQL.squish
       DROP TABLE page_views_counters_by_context_and_user;
-    }
+    SQL
   end
 end
