6d5204ec0cc442eb9e3de4f4576011e82f7b8950
Cody Cutrer <cody@instructure.com>
2021-11-22 10:30:38 -0700

    RuboCop: Style/SymbolArray, Style/WordArray
    auto-corrected
    
    Change-Id: I4b7d633edda8b542304f67fd4f1119cb7ae8a0bf
    Reviewed-on: https://gerrit.instructure.com/c/analytics/+/279182
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Jacob Burroughs <jburroughs@instructure.com>
    QA-Review: Cody Cutrer <cody@instructure.com>
    Product-Review: Cody Cutrer <cody@instructure.com>
    Migration-Review: Cody Cutrer <cody@instructure.com>

--- db/migrate/20160119183554_make_page_views_rollups_unique.rb
+++ db/migrate/20160119183554_make_page_views_rollups_unique.rb
@@ -5,8 +5,8 @@ class MakePageViewsRollupsUnique < ActiveRecord::Migration[4.2]
   disable_ddl_transaction!
 
   def up
-    scope = PageViewsRollup.select([:course_id, :date, :category]).group(:course_id, :date,
-                                                                         :category).having("COUNT(*) > 1")
+    scope = PageViewsRollup.select(%i[course_id date category]).group(:course_id, :date,
+                                                                      :category).having("COUNT(*) > 1")
     GuardRail.activate(:secondary) do
       scope.find_each do |dup|
         GuardRail.activate(:primary) do
@@ -18,14 +18,14 @@ class MakePageViewsRollupsUnique < ActiveRecord::Migration[4.2]
 
     rename_index :page_views_rollups, 'index_page_views_rollups_on_course_id_and_date_and_category',
                  'index_page_views_rollups_deprecated'
-    add_index :page_views_rollups, [:course_id, :date, :category], unique: true, algorithm: :concurrently
+    add_index :page_views_rollups, %i[course_id date category], unique: true, algorithm: :concurrently
     remove_index :page_views_rollups, name: 'index_page_views_rollups_deprecated'
   end
 
   def down
     rename_index :page_views_rollups, 'index_page_views_rollups_on_course_id_and_date_and_category',
                  'index_page_views_rollups_deprecated'
-    add_index :page_views_rollups, [:course_id, :date, :category], algorithm: :concurrently
+    add_index :page_views_rollups, %i[course_id date category], algorithm: :concurrently
     remove_index :page_views_rollups, name: 'index_page_views_rollups_deprecated'
   end
 end
